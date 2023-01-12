fb708d7525800751c0e7e76bb3531aaf81a29024
Myeonghak Lee <mhmh779@naver.com>
2023-01-12 16:21:16 +0900

    practice-Mixins

--- blog/views.py
+++ blog/views.py
@@ -1,59 +1,39 @@
-from django.shortcuts import render
-
-# Create your views here.
 from .models import Blog
 from .serializer import BlogSerializer
-# APIView를 사용하기 위해 import
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.http import Http404
+from rest_framework import generics
+from rest_framework import mixins
 
-# Blog list를 보여줄 때
-class BlogList(APIView):
+# Blog의 목록을 보여주는 역할
+class BlogList(mixins.ListModelMixin,
+                mixins.CreateModelMixin,
+                generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-  def get(self, request):
-    blogs = Blog.objects.all()
-    # 여러 개의 객체를 serialization하기 위해 many = True로 설정
-    serializer = BlogSerializer(blogs, many = True)
-    return Response(serializer.data)
+    # Blog list를 보여줄 때
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-  # 새로운 blog 글을 작성할 때 
-  def post(self, request):
-    # request.data는 사용자의 입력 데이터
-    serializer = BlogSerializer(data=request.data)
-    if serializer.is_valid(): # 유효성 검사
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # 새로운 Blog 글을 작성 할 때 
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-# Blog detail을 보여주는 역할
-class BlogDetail(APIView):
-  
-  # blog 객체 가져오기
-  def get_object(self, pk): # get_object 선언
-    try:
-      return Blog.objects.get(pk=pk)
-    except Blog.DoesNotExist:
-      raise Http404
-  
-  # blog의 detail 보기
-  def get(self, request, pk, format=None):
-    blog = self.get_object(pk)
-    serializer = BlogSerializer(blog)
-    return Response(serializer.data)
+# Blog의 detail을 보여주는 역할
+class BlogDetail(mixins.RetrieveModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.DestroyModelMixin,
+                generics.GenericAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-  # blog 수정하기
-  def put(self, request, pk, format=None):
-    blog = self.get_object(pk)
-    serializer = BlogSerializer(blog, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data)
-    return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-  
-  # blog 삭제하기
-  def delete(self, request, pk, format=None):
-    blog = self.get_object(pk)
-    blog.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
+    # Blog의 detail 보기
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    # Blog 수정하기
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    # Blog 삭제하기
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
