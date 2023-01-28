ea6aecc2e9476980fc93afef1f34206b5c6588a7
Myeonghak Lee <mhmh779@naver.com>
2023-01-28 23:41:16 +0900

    add Permission

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,14 @@ from .models import Blog
 from .serializer import BlogSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Blog의 목록, detail 보여주기, 수정하기, 삭제하기 모두 가능
 class BlogViewSet(viewsets.ModelViewSet):
     # authentication 추가
     authentication_classes = [BasicAuthentication, SessionAuthentication]
+    # permission 추가
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
 
